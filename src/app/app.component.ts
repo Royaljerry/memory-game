66ee9988d4f98ac34cd872343f8e1ddd28de70f3
Adam Pócs <adam.pocs@edreamsodigeo.com>
2019-12-07 22:34:25 +0100

    Shit happens. Cannnot load JSON file. Whaddafukk.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Memory Game';
-  numberOfGroups = 3;
+export class AppComponent implements OnInit{
+  title: string;
+  numberOfGroups: number;
+  hello: string;
+
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(){
+    this.httpClient.get('../assets/data.json').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
