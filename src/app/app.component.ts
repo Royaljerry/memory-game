4c71bb2f9a767afbcf9d6e24532f4804a6d7bc25
Adam Pócs <adam.pocs@edreamsodigeo.com>
2019-12-11 10:13:34 +0100

    Problems with json loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,14 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title: string;
   numberOfGroups: number;
   hello: string;
 
   constructor(private httpClient: HttpClient) {}
-  ngOnInit(){
-    this.httpClient.get('../assets/data.json').subscribe(data => {
+  ngOnInit() {
+    this.httpClient.get('assets/data.json').subscribe(data => {
       console.log(data);
     });
   }
