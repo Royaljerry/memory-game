edaa3bbbf99f58ad0f3a7dda8084225960ab31a4
Adam Pócs <adam.pocs@edreamsodigeo.com>
2020-01-04 23:35:18 +0100

    Fuck. Still does not work. Let’s start it again next time.

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -11,33 +11,15 @@ export class CardComponent implements OnInit {
 
   @Input() card: Card;
   @Input() sideLengths: number[];
+  width: string;
+  height: string;
+  maxWidth: string;
+  maxHeight: string;
 
   @Output() turnedChanged: EventEmitter<boolean> = new EventEmitter();
 
   constructor() {
 
-  }
-
-  setupCardDimensions(sideLengths) {
-    const sideLengthMin = sideLengths[0];
-    const sideLengthMax = sideLengths[1];
-    console.log(RjViewport.isPortrait, sideLengthMin, sideLengthMax);
-    // switch (RjViewport.isPortrait) {
-    //   case true:
-    //     return {
-    //       width: `100vw - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`,
-    //       height: `100vw - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`,
-    //       maxWidth: `100vh - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`,
-    //       maxHeight: `100vh - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`
-    //     };
-    //   case false:
-    //     return {
-    //       width: `100vw - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`,
-    //       height: `100vw - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`,
-    //       maxWidth: `100vh - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`,
-    //       maxHeight: `100vh - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`
-    //     };
-    // }
   }
 
   getImagePath(card: Card) {
@@ -53,6 +35,24 @@ export class CardComponent implements OnInit {
     }
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const sideLengthMin = this.sideLengths[0];
+    const sideLengthMax = this.sideLengths[1];
+    console.log(RjViewport.isPortrait, sideLengthMin, sideLengthMax);
+    switch (RjViewport.isPortrait) {
+      case true:
+        this.width = `100vw - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`;
+        this.height = `100vw - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`;
+        this.maxWidth = `100vh - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`;
+        this.maxHeight = `100vh - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`;
+        break;
+      case true:
+        this.width = `100vw - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`;
+        this.height = `100vw - ${sideLengthMax - 1} * 16px) / ${sideLengthMax})`;
+        this.maxWidth = `100vh - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`;
+        this.maxHeight = `100vh - ${sideLengthMin - 1} * 16px) / ${sideLengthMin})`;
+        break;
+    }
+  }
 
 }
